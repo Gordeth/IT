f0d9f7766e68f1401391851ac359b6419c384304
Gordeth <luis_barbudo@msn.com>
2025-06-02 16:19:37 +0100

    Delete winget-upgrade.ps1

--- winget-upgrade.ps1
+++
@@ -1,9 +0,0 @@
-# winget-upgrade.ps1
-
-Write-Host "Checking for package updates with winget..." -ForegroundColor Cyan
-try {
-    winget upgrade --all --silent
-    Write-Host "All packages updated successfully." -ForegroundColor Green
-} catch {
-    Write-Host "Failed to update packages. Error: $_" -ForegroundColor Red
-}
