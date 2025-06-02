94a4e972b12d0197f5897b2750453e93e316e9d8
Gordeth <luis_barbudo@msn.com>
2025-06-02 10:48:00 +0100

    Update RunWindowsUpdate.bat

--- RunWindowsUpdate.bat
+++ RunWindowsUpdate.bat
@@ -31,7 +31,8 @@ if exist "%SCRIPT_DIR%\WindowsUpdateScript.ps1" (
 ) else (
     echo Downloading WindowsUpdateScript.ps1 from GitHub...
 )
-powershell -Command "Invoke-WebRequest -Uri %BASE_URL%/WindowsUpdateScript.ps1 -OutFile %SCRIPT_DIR%\WindowsUpdateScript.ps1 -UseBasicParsing"
+# powershell -Command "Invoke-WebRequest -Uri %BASE_URL%/WindowsUpdateScript.ps1 -OutFile %SCRIPT_DIR%\WindowsUpdateScript.ps1 -UseBasicParsing"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\WindowsUpdateScript.ps1" >> "%LOG_FILE%" 2>&1
 
 :: Download or update winget-upgrade.ps1
 echo Checking for winget-upgrade.ps1 in %SCRIPT_DIR%...
