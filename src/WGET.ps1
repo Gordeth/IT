f99248a002b1bd67525fdab03531ae052594115a
Gordeth <luis_barbudo@msn.com>
2025-08-22 17:21:24 +0100

    docs(wget): improve script documentation
    - Added detailed help comments
    - Included parameter descriptions
    - Added version and dependencies info
    - Improved changelog section
    - Updated script synopsis and description

--- src/WGET.ps1
+++ src/WGET.ps1
@@ -1,20 +1,30 @@
-# WGET.ps1
-# Version: 1.0
-
-# Description: This script automates the update of all installed packages using the winget package manager.
-#              It provides options for verbose logging and specifies a log file for detailed activity tracking.
-#              The script ensures the logging environment is set up correctly before execution and handles
-#              the winget upgrade process with error handling.
-#
-# Changelog:
-# ---------------------------------------------------------------------------------------------------
-# Version 1.0 - 2025-08-22
-#   - Initial release.
-#   - Implemented winget upgrade --all with verbose and logging options.
-#   - Added log directory and file creation.
-#   - Integrated with Functions.ps1 for logging.
-# ---------------------------------------------------------------------------------------------------
-
+<#
+.SYNOPSIS
+    Automates winget package updates.
+.DESCRIPTION
+    This script automates the update of all installed packages using the winget package manager.
+    It provides options for verbose logging and specifies a log file for detailed activity tracking.
+    The script ensures the logging environment is set up correctly before execution and handles
+    the winget upgrade process with error handling.
+.PARAMETER VerboseMode
+    If specified, enables detailed logging output to the console.
+    Otherwise, only ERROR level messages are displayed on the console.
+.PARAMETER LogDir
+    The full path to the log directory where all script actions will be recorded.
+    This parameter is mandatory.
+.NOTES
+    Script: WGET.ps1
+    Version: 1.0
+    Dependencies:
+        - winget (Windows Package Manager) must be installed and configured.
+        - Internet connectivity
+    Changelog:
+        v1.0
+        - Initial release.
+        - Implemented winget upgrade --all with verbose and logging options.
+        - Added log directory and file creation.
+        - Integrated with Functions.ps1 for logging.
+#>
 param (
   # Parameter passed from the orchestrator script (WUH.ps1) to control console verbosity.
   [switch]$VerboseMode = $false,
@@ -74,7 +84,7 @@ if (-not (Test-Path $LogFile)) {
 # Log an informational message indicating the start of the script.
 Log "Starting WGET.ps1 script."
 
-# Log the action of checking for available package updates using winget.
+# Log the action of checking for available package updates with winget.
 Log "Checking for package updates with winget..."
 
 # Attempt to execute the `winget upgrade --all` command to update all installed packages.
